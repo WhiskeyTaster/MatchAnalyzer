4cdf78979f503bbb96bb6ce1ee8ca56622f483b9
WhiskeyTaster <79936887+WhiskeyTaster@users.noreply.github.com>
2023-01-19 16:42:12 +0100

    Init commit

---
+++ src/main/java/com/whiskeytaster/matchanalyzer/MatchanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.whiskeytaster.matchanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchanalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whiskeytaster/matchanalyzer/MatchanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiskeytaster.matchanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
